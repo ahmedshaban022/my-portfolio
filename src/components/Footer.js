188c2d6db87e8d1144d83e288e662b6bd8febbe3
ahmedshaban022 <ahmedshaban.ua007@gmail.com>
2022-06-22 15:05:48 +0200

    Change Footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer(props) {
           <div className="container-lg">
             <div className="row">
               <div className="col-lg-12">
-                <p className="m-0 text-muted text-center">© 2021 The Website</p>
+                <p className="m-0 text-muted text-center">𝒜ℋℳℰ𝒟 𝒮ℋ𝒜ℬ𝒜𝒩</p>
               </div>
             </div>
           </div>
